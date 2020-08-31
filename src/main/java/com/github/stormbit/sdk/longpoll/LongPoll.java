0ac408ae91c3c849c281073a7ec62d2cb92aa81f
Storm-bit <carpowroma@gmail.com>
2020-08-31 23:02:56 +0300

    update

--- src/main/java/com/github/stormbit/sdk/longpoll/LongPoll.java
+++ src/main/java/com/github/stormbit/sdk/longpoll/LongPoll.java
@@ -13,7 +13,6 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.Map;
 
